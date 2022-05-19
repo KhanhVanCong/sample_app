3b59897540095a1e80d793f8021200e2aeeec073
khanhvancong <khanhvc2310@gmail.com>
2022-05-19 15:55:37 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX},
+            format: { with: VALID_EMAIL_REGEX },
             uniqueness: true
 
   has_secure_password
@@ -48,13 +48,27 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  private
-  def downcase_email
-    email.downcase!
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns({ reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now })
   end
 
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
